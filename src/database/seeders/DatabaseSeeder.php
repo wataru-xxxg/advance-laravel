19e120f6f47084aeac8c4699d00135b7f30bc9a7
nishida wataru <xxxg.01d2.jannedaarc@gmail.com>
2025-03-08 23:25:24 +0900

    uuid

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Author;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Author::factory(10)->create();
+        Product::factory(10)->create();
     }
 }
